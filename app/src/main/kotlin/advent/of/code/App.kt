a2d9393fd341fdaf3a29f40b9c2efb160122f3a1
Stefan Salanski <stefan.salanski@gmail.com>
2022-12-01 12:34:42 -0500

    example input and solution for day 1 part 1

--- app/src/main/kotlin/advent/of/code/App.kt
+++ app/src/main/kotlin/advent/of/code/App.kt
@@ -3,6 +3,9 @@
  */
 package advent.of.code
 
+import java.io.BufferedReader
+import java.lang.Integer.max
+
 class App {
     val greeting: String
         get() {
@@ -10,6 +13,29 @@ class App {
         }
 }
 
+class AdventOfCode {
+
+    fun `day 1 part 1`(runOnExample: Boolean = false) {
+        val filename = if (runOnExample) "example.txt" else "input.txt"
+        var currentElf = 0
+        var maxElf = 0
+        readInput("/day01/$filename").forEachLine {
+            if (it.isEmpty()) {
+                currentElf = 0
+            } else {
+                currentElf += it.toInt()
+            }
+            maxElf = max(maxElf,currentElf)
+        }
+        println("highest calorie elf has $maxElf calories")
+    }
+    private fun readInput(name: String): BufferedReader =
+        this.javaClass::class.java.getResource(name)?.openStream()?.bufferedReader() ?: throw IllegalArgumentException("$name was not found")
+}
 fun main() {
     println(App().greeting)
+
+    AdventOfCode().`day 1 part 1`(true)
+    AdventOfCode().`day 1 part 1`()
+
 }
