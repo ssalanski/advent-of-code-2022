3e9e34569a1139c7be3d9deb9ebb5e597dbceab0
Stefan Salanski <stefan.salanski@sapns2.com>
2022-12-02 10:19:45 -0500

    add abstract val `day` to the AdventOfCode interface, use it to construct full filename path automatically

--- app/src/main/kotlin/advent/of/code/Day01.kt
+++ app/src/main/kotlin/advent/of/code/Day01.kt
@@ -1,10 +1,12 @@
 package advent.of.code
 
 class Day01(runOnExample: Boolean = false) : AdventOfCode(runOnExample) {
+  override val day: Int = 1
+
   override fun partOne() {
     var currentElf = 0
     var maxElf = 0
-    readInput("/day01/$filename").forEachLine {
+    readInput().forEachLine {
       if (it.isEmpty()) {
         currentElf = 0
       } else {
@@ -18,7 +20,7 @@ class Day01(runOnExample: Boolean = false) : AdventOfCode(runOnExample) {
   override fun partTwo() {
     var currentElf = 0
     val maxElves = TopNKeeper(3)
-    readInput("/day01/$filename").forEachLine {
+    readInput().forEachLine {
       if (it.isEmpty()) {
         maxElves.consider(currentElf)
         currentElf = 0
